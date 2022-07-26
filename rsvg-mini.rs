5479f9121b8796e8e644a6c912f550eb11ad4860
biojet1 <biojet1@gmail.com>
2022-07-26 22:25:49 +0800

    TreeBuilder

--- rsvg-mini.rs
+++ rsvg-mini.rs
@@ -1,3 +1,18 @@
+use crate::prefixes::{Prefix, Prefixes};
+// use crate::{Element, Error};
+use rxml::RawEvent;
+
+/// Tree-building parser state
+pub struct TreeBuilder {
+    // next_tag: Option<(Prefix, String, Prefixes, BTreeMap<String, String>)>,
+    // Parsing stack
+    // stack: Vec<Element>,
+    // Namespace set stack by prefix
+    prefixes_stack: Vec<Prefixes>,
+    // Document root element if finished
+    // pub root: Option<Element>,
+}
+
 fn main() {
     use rxml::EventRead;
     let doc = b"<?xml version='1.0'?><hello>World!</hello>";
